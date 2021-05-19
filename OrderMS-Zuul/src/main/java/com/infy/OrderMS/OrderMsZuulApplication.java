556cdc1a918c2e06536ef0458903930a670aae40
Rajashree-Rajagopal <84319274+Rajashree-Rajagopal@users.noreply.github.com>
2021-05-19 21:46:46 +0530

    Zuul upload

---
+++ OrderMS-Zuul/src/main/java/com/infy/OrderMS/OrderMsZuulApplication.java
@@ -0,0 +1,17 @@
+package com.infy.OrderMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class OrderMsZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMsZuulApplication.class, args);
+	}
+
+}
